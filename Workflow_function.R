108c0c05ebdd645eedd7a71927756ab75782c8bd
Chronostasis <ngzh5554@leeds.ac.uk>
2025-05-01 17:37:41 +0100

    Changed the function for the RF and XGBoost. And Changed to use the xgboost package to train the XGBoost Model.Still need to check and test again.Also another problem is that the number of cross validation is different between RF and XGBoost.

--- Workflow_function.R
+++ Workflow_function.R
@@ -146,13 +146,15 @@ SplitDataSet <- function(CrcBiomeScreenObject = NULL,
   # Add labels to the training and test sets
   train_label <- sample_condition[trainIndex]
   test_label <- sample_condition[-trainIndex]
-  train[[condition_col]] <- as.factor(train_label)
-  test[[condition_col]] <- as.factor(test_label)
+#   train[[condition_col]] <- as.factor(train_label)
+#   test[[condition_col]] <- as.factor(test_label)
   
   # Create a list to store the training and test sets
   ModelData <- list(
     Training = train,
-    Test = test
+    Test = test,
+    TrainLabel = train_label,
+    TestLabel = test_label
   )
   
   # Add attributes to the ModelData
@@ -168,6 +170,47 @@ SplitDataSet <- function(CrcBiomeScreenObject = NULL,
   return(CrcBiomeScreenObject)
 }
 
+
+RunModeling <- function(CrcBiomeScreenObject, 
+                        model_type = c("RF", "XGBoost"), 
+                        n_cv = 10, 
+                        task_name = "ModelingTask", 
+                        TrueLabel = NULL) {
+  # Initialize result storage
+  results <- list()
+  
+  # Check the input
+  if (is.null(CrcBiomeScreenObject$ModelData)) {
+    stop("ModelData is missing in CrcBiomeScreenObject. Please run SplitDataSet first.")
+  }
+  
+  # Run the RF model
+  if ("RF" %in% model_type) {
+    rf_result <- RF_Modeling(
+      ModelData = CrcBiomeScreenObject$ModelData,
+      k.rf = n_cv,
+      TaskName = task_name,
+      TrueLabel = TrueLabel
+    )
+    results$RF <- rf_result
+  }
+  
+  # Run the XGBoost model
+  if ("XGBoost" %in% model_type) {
+    xgb_result <- XGBoost_Modeling(
+      ModelData = CrcBiomeScreenObject$ModelData,
+      TaskName = task_name,
+      TrueLabel = TrueLabel
+    )
+    results$XGBoost <- xgb_result
+  }
+  
+  # Save the result into the CrcBiomeScreenObject
+  CrcBiomeScreenObject$ModelResult <- results
+  saveRDS(CrcBiomeScreenObject, paste0("CrcBiomeScreenObject_", task_name, ".rds"))
+  
+  return(CrcBiomeScreenObject)
+}
 
 
 #' Random Forest Model
@@ -187,79 +230,189 @@ SplitDataSet <- function(CrcBiomeScreenObject = NULL,
 #' 
 #' @return best.params.rf The best parameters for model
 #'
-RF_Modeling <- function(ModelData = NULL,
-                        label = NULL,
+RF_Modeling <- function(CrcBiomeScreenObject = NULL,
                         k.rf = NULL,
-                        taxa_col = NULL,
                         TaskName = NULL,
-                        TrueLabel = NULL
-                        ){
-    set.seed(123)
-    folds.rf <- createFolds(get(label,ModelData[[1]]), k = k.rf)
-    
-    # Using ranger random forest for faster implementation
-    grid.rf <- expand.grid(
-    mtry         = seq(5,25, by = 5),
-    node_size    = seq(3, 15, by = 2),
-    sample_size  = c(.55, .632, .70, .80),
-    num.trees    = seq(600, 1000, by = 150),
-    AUC     = 0
-    )                        
+                        TrueLabel = NULL) {
+  set.seed(123)
+  folds.rf <- createFolds(CrcBiomeScreenObject$ModelData$TrainLabel, k = k.rf)
+  
+  # Calculate the number of cores
+  # num_cores <- 10
+  num_cores <- detectCores() - 20
+  cl <- makePSOCKcluster(num_cores)
+  registerDoParallel(cl)
+  
+  # tuneGrid for ranger
+  grid.rf <- expand.grid(
+    mtry = seq(5, 25, by = 5),
+    node_size = seq(3, 15, by = 2),
+    sample_size = c(.55, .632, .70, .80),
+    num.trees = seq(600, 1000, by = 150),
+    AUC = 0
+  )
+  
+  # Using ranger random forest for faster implementation
+  grid.rf$AUC <- foreach(i = 1:nrow(grid.rf), .combine = c, .packages = c("ranger", "pROC")) %dopar% {
+    aucs <- sapply(1:k.rf, function(j) {
+      val.indices <- folds.rf[[j]]
+      val.data <- CrcBiomeScreenObject$ModelData$Training[val.indices, ]
+      train.fold.data <- CrcBiomeScreenObject$ModelData$Training[-val.indices, ]
+      train.fold.data$TrainLabel <- as.factor(CrcBiomeScreenObject$ModelData$TrainLabel[-val.indices])
 
-    for(i in 1:nrow(grid.rf)) {
-
-    aucs <- c()  # Initialising a vector to store AUCs for each fold
-
-    for(j in 1:k.rf) {
-        val.indices <- folds.rf[[j]]  # Indices for the current fold
-        val.data <- ModelData[[1]][val.indices, ]  # Validation data for the current fold
-        train.fold.data <- ModelData[[1]][-val.indices, ]  # Training data for the current fold
-        
-        # Class weights in each fold
-        class_weights <- table(get(label,train.fold.data))  # Calculate the number of samples in each class
-        class_weights <- sum(class_weights) / (length(class_weights) * class_weights)  # Calculate the weight
-
-        colnames(train.fold.data) <- make.names(colnames(train.fold.data)) # nolint
-        colnames(val.data) <- make.names(colnames(val.data))
-        loc <- which(label == colnames(train.fold.data))
-
-        # Model training with the specified hyperparameters
-        model <- ranger(
-        formula         = as.formula(paste(label, "~ .")), 
-        data            = train.fold.data[,c(taxa_col,loc)], 
-        num.trees       = grid.rf$num.trees[i],
-        mtry            = grid.rf$mtry[i],
-        min.node.size   = grid.rf$node_size[i],
+      # Class weights in each fold
+      class_weights <- table(train.fold.data$TrainLabel)
+      class_weights <- sum(class_weights) / (length(class_weights) * class_weights)
+      
+      # Model training with the specified hyperparameters
+      model <- ranger(
+        formula = as.formula(paste("TrainLabel ~ .")),
+        data = train.fold.data,
+        num.trees = grid.rf$num.trees[i],
+        mtry = grid.rf$mtry[i],
+        min.node.size = grid.rf$node_size[i],
         sample.fraction = grid.rf$sample_size[i],
-        class.weights   = class_weights,  
-        seed            = 123,
-        classification  = TRUE,
-        probability     = TRUE,
-        verbose         = FALSE
-        )
-        
-        # Prediction on validation data
-        predictions <- predict(model, data = val.data[,taxa_col], type = "response")$predictions
-
-        # Computing AUC
-        roc.obj <- roc(get(label,val.data), predictions[, TrueLabel])
-        auc <- auc(roc.obj)
-        
-        # AUC on the current fold
-        aucs <- c(aucs, auc)
-    }
-
-    # Averaging AUC for the current hyperparameter combination
-    grid.rf$AUC[i] <- mean(aucs)
-    }
-    # saveRDS(grid.rf,paste0("grid.rf_",TaskName,".rds"))
-    best.params.index.rf <- which.max(grid.rf$AUC)
-    best.params.rf <- grid.rf[best.params.index.rf, ]
-    saveRDS(best.params.rf,paste0("best.params.rf_",TaskName,".rds"))
-    print("Save the result sucessfully!")
+        class.weights = class_weights,
+        seed = 123,
+        classification = TRUE,
+        probability = TRUE,
+        verbose = FALSE
+      )
+      # Validation data prediction
+      predictions <- predict(model, data = val.data, type = "response")$predictions
+      val.Label <- CrcBiomeScreenObject$ModelData$TrainLabel[val.indices]
+      roc.obj <- roc(val.Label, predictions[, TrueLabel])
+      auc(roc.obj)
+    })
     
-    return(best.params.rf)
+    # AUC on the current fold
+    mean(aucs)
+  }
+  
+  # Stop the cluster
+  stopCluster(cl)
+  registerDoSEQ()
+  
+  # Choose the best parameters
+  best.params.index.rf <- which.max(grid.rf$AUC)
+  best.params.rf <- grid.rf[best.params.index.rf, ]
+  # Save the best parameters
+  CrcBiomeScreenObject$ModelResult$RF <- list(grid.para = grid.rf, best.params = best.params.rf)
+
+  # saveRDS(best.params.rf, paste0("best.params.rf_", TaskName, ".rds"))
+  
+  print("Save the result successfully!")
+  return(CrcBiomeScreenObject)
 }
+
+
+XGBoost_Modeling <- function(CrcBiomeScreenObject = NULL,
+                             TaskName = NULL,
+                             TrueLabel = NULL) {
+  set.seed(123)
+  
+  # 检查输入
+  if (is.null(CrcBiomeScreenObject$ModelData)) {
+    stop("ModelData is missing in CrcBiomeScreenObject. Please run SplitDataSet first.")
+  }
+  
+  # 设置并行计算
+  num_cores <- detectCores() - 1  # 使用可用核心数减1，避免占满所有核心
+  cl <- makePSOCKcluster(num_cores)
+  registerDoParallel(cl)
+  
+  # 定义超参数网格
+  grid.xgb <- expand.grid(
+    nrounds = c(100, 200, 300),
+    max_depth = c(3, 5, 7, 9),
+    eta = c(0.01, 0.1, 0.3),
+    gamma = 0,
+    colsample_bytree = c(0.5, 0.75, 1),
+    min_child_weight = 1,
+    subsample = c(0.5, 0.75, 1),
+    AUC = 0
+  )
+  
+  label_train <- ifelse(CrcBiomeScreenObject$ModelData$TrainLabel == TrueLabel, 1, 0)
+  label_test <- ifelse(CrcBiomeScreenObject$ModelData$TestLabel == TrueLabel, 1, 0)
+
+  dtrain <- xgb.DMatrix(data = as.matrix(CrcBiomeScreenObject$ModelData$Training), label = label_train)
+  dtest <- xgb.DMatrix(data = as.matrix(CrcBiomeScreenObject$ModelData$Test), label = label_test)
+  
+  for(i in seq_len(nrow(grid.xgb))) {
+    params <- list(
+      objective = "binary:logistic",
+      eval_metric = "auc",
+      max_depth = grid.xgb$max_depth[i],
+      eta = grid.xgb$eta[i],
+      gamma = grid.xgb$gamma[i],
+      colsample_bytree = grid.xgb$colsample_bytree[i],
+      min_child_weight = grid.xgb$min_child_weight[i],
+      subsample = grid.xgb$subsample[i]
+    )
+    # cross-validation
+    cv <- xgb.cv(
+      params = params,
+      data = dtrain,
+      nrounds = grid.xgb$nrounds[i],
+      nfold = 10,
+      verbose = TRUE,
+      prediction = TRUE,
+      showsd = TRUE
+    )
+    
+    # 返回 AUC
+    max(cv$evaluation_log$test_auc_mean)
+  }
+
+  # Stop the cluster
+  stopCluster(cl)
+  registerDoSEQ()
+  
+  # Select the best parameters
+  best.params.index.xgb <- which.max(grid.xgb$AUC)
+  best.params.xgb <- grid.xgb[best.params.index.xgb, ]
+  
+  # 使用最佳参数重新训练模型
+  best.params <- list(
+    objective = "binary:logistic",
+    eval_metric = "auc",
+    max_depth = best.params.xgb$max_depth,
+    eta = best.params.xgb$eta,
+    gamma = best.params.xgb$gamma,
+    colsample_bytree = best.params.xgb$colsample_bytree,
+    min_child_weight = best.params.xgb$min_child_weight,
+    subsample = best.params.xgb$subsample
+  )
+  
+  xgb_model <- xgb.train(
+    params = best.params,
+    data = dtrain,
+    nrounds = best.params.xgb$nrounds,
+    verbose = FALSE
+  )
+  
+  # Test the model
+  test.pred.prob.xgb <- predict(xgb_model, newdata = dtest)
+  
+  # Calculate AUC
+  roc.curve.xgb <- roc(label_test, test.pred.prob.xgb)
+  auc.value.xgb <- auc(roc.curve.xgb)
+  
+  # Save the results
+  CrcBiomeScreenObject$ModelResult$XGBoost <- list(
+    BestParams = best.params.xgb,
+    Model = xgb_model,
+    ROC = roc.curve.xgb,
+    AUC = auc.value.xgb
+  )
+  
+  return(CrcBiomeScreenObject)
+}
+
+
+
+
 
 
 #' Test the Model
