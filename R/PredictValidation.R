602ecb02db43d7cef620a1dc01701c70a4a0eef7
Chronostasis <ngzh5554@leeds.ac.uk>
2025-05-08 17:12:07 +0100

    ADD PredictValidation.R file.

---
+++ R/PredictValidation.R
@@ -0,0 +1,64 @@
+PredictValidation <- function(
+  CrcBiomeScreenObject = NULL,
+  model_type = NULL,
+  ValidationData = NULL,
+  TaskName = NULL,
+  condition_col = "study_condition",
+  PlotAUC = NULL
+) {
+  if (!condition_col %in% colnames(ValidationData$SampleData)) {
+    stop(paste("Condition column", condition_col, "not found in SampleData."))
+  }
+    # Load the model
+    if(model_type == "RF") {
+        rf.model <- CrcBiomeScreenObject$ModelResult$RF$EvaluateResult$RF.Model
+        probs.ValidationData.rf <- predict(rf.model, newdata = ValidationData$NormalizedData, type = 'response')$predictions
+        # Actual labels
+        actual.classes.rf <- as.factor(ValidationData$SampleData$study_condition)
+        # calculating the ROC Curve
+        roc.curve.rf <- roc(actual.classes.rf, probs.ValidationData.rf, levels = levels(as.factor(ValidationData$SampleData$study_condition))
+        # Plot
+        if(PlotAUC == TRUE){
+            pdf(paste0("roc.curve.rf.",TaskName,".pdf"))
+            plot(roc.curve.rf, print.auc = TRUE, print.thres = TRUE)
+            dev.off()
+        }
+        CrcBiomeScreenObject$PredictResult[["RF"]][[TaskName]] <- 
+        list(roc.curve = roc.curve.rf,
+             AUC = auc(roc.curve.rf))
+
+    } else if(model_type == "XGBoost") {
+        xgb.model <- CrcBiomeScreenObject$ModelResult$EvaluateResult$XGBoost.Model
+        label_ValidationData <- ifelse(CrcBiomeScreenObject$SampleData$study_condition == TrueLabel, 1, 0)
+        ValidationData <- xgb.DMatrix(data = as.matrix(ValidationData$NormalizedData), label = ValidationData$SampleData$study_condition)
+        # Test the model
+        pred.prob.xgb <- predict(xgb.model, newdata = ValidationData)[[TrueLabel]]
+        # Calculate AUC
+        roc.curve.xgb <- roc(label_ValidationData, pred.prob.xgb)
+        auc.value.xgb <- auc(roc.curve.xgb)
+        
+        # Plot the ROC curve
+        if(PlotAUC == TRUE){
+            pdf(paste0("roc.curve.xgb.",TaskName,".pdf"))
+            plot(roc.curve.xgb, print.auc = TRUE, print.thres = TRUE)
+            dev.off()
+        }
+       CrcBiomeScreenObject$PredictResult[["XGBoost"]][[TaskName]] <- 
+        list(roc.curve = roc.curve.xgb,
+             AUC = auc(roc.curve.xgb))
+
+    } else {
+        stop("Invalid model type. Please choose either 'RF' or 'XGBoost'.")
+    }
+  # Save the result
+  saveRDS(CrcBiomeScreenObject, paste0("CrcBiomeScreenObject_", TaskName, ".rds"))
+  print("Save the result successfully!")
+  return(CrcBiomeScreenObject)
+}
+
+
+
+
+
+
+
