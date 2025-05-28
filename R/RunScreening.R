10c8f83bfbec690840916e2fa239bcdaa438bf99
Chronostasis <ngzh5554@leeds.ac.uk>
2025-05-28 16:42:14 +0100

    Update the RunScreening.R file.

--- R/RunScreening.R
+++ R/RunScreening.R
@@ -1,10 +1,21 @@
 
-RunScreening <- function(obj, normalize_method = "TSS", model = "RF", split.requirement = c(label = c("control","CRC"), partition = 0.7), TaskName = "GMPR_NHSBCSP", num_cores = 10) {
+RunScreening <- function(obj,
+                         normalize_method = "TSS", 
+                         model = "RF", 
+                         split.requirement = NULL, #c(label = c("control","CRC")  
+                         TaskName = TaskName, 
+                         ValidationData = NULL,
+                         TrueLabel = TrueLabel,
+                         num_cores = num_cores) {
+
   obj <- NormalizeData(obj, method = normalize_method)
-  obj <- SplitDataSet(obj, split.requirement)
+  obj <- SplitDataSet(obj, split.requirement,partition = 0.7)
 
   obj <- TrainModels(obj, model_type = model, TaskName = TaskName, TrueLabel = "CRC", num_cores = num_cores)
   obj <- EvaluateModel(obj, model_type = model, TaskName = paste0(TaskName, "_Test"), TrueLabel = "CRC", PlotAUC = TRUE)
+
+  obj <- ValidateModelOnData(obj, model_type = model, ValidationData = ValidationData, TaskName = paste0(TaskName, "_Validation"), TrueLabel = TrueLabel, PlotAUC = TRUE)
+  
   return(obj)
 }
 
